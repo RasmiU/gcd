0fbff35b718b1818f1e200ecdb34e1afc4080bdd
Rasmiusankar13hp <33249675+Rasmiusankar13hp@users.noreply.github.com>
2017-10-31 02:11:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int gcd(int a,int b)
+ #include <stdio.h>
+int gcd(int a,int b)        //recursive function
 {
-    if(b==0) return a;
+    if(b==0) return a;      //a contains the value of the gcd
     return gcd(b,a%b);
 }
 
 int main()
 {
     int x,y;
-    scanf("%d%d",&x,&y);
+    scanf("%d %d",&x,&y);
     int g;
     g=gcd(x,y);
     printf("%d",g);
